1ea76fc25ff3e8efc4066f58a70c8167dc67c9cd
William Melody <wmelody@gmail.com>
2009-11-05 21:08:03 -0600

    Too aggresive of a regex:
    * (java|vb)?script was catching 'description'
    * [\<>] was catching 'body > div'
    
    Conflicts:
    
    	lib/css_sanitize.rb

--- lib/css_sanitize.rb
+++ lib/css_sanitize.rb
@@ -15,7 +15,7 @@ module CssSanitize
     evil = [
       /(\bdata:\b|eval|cookie|\bwindow\b|\bparent\b|\bthis\b)/i, # suspicious javascript-type words
       /behaviou?r|expression|moz-binding|@import|@charset|(java|vb)?script|[\<]|\\\w/i,
-      /[\<>]/, # back slash, html tags,
+      /(\<.+>)/, # back slash, html tags,
       /[\x7f-\xff]/, # high bytes -- suspect
       /[\x00-\x08\x0B\x0C\x0E-\x1F]/, #low bytes -- suspect
       /&\#/, # bad charset
